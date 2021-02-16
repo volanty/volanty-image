916b42998f27fa3e3034a562841b258e9f6d2875
Inaiat H. Moraes <inaiat@gmail.com>
2021-02-16 20:51:30 -0300

    refactoring

--- scripts/deployer/scripts/deploy_helm.py
+++ scripts/deployer/scripts/deploy_helm.py
@@ -14,28 +14,29 @@ from ..utils import *
 @click.argument("tag", default=os.getenv("BITBUCKET_COMMIT"))
 @click.argument("deploy-env", default=os.getenv("BITBUCKET_DEPLOYMENT_ENVIRONMENT"))
 @click.option("--gcp-cluster", "-g", default="elasticsearch-cluster", help="gcp k8s cluster name")
-@click.option("--aws-credentials-csv-file", "-c", default=os.getenv("AWS_K8S_CREDENTIALS"), help="the aws credentials csv file")
+@click.option("--aws-credentials", "-c", default="/tmp/aws-credentials.json", help="the aws credentials csv file")
 @click.option("--aws-region", "-r", default="us-west-2", help="the aws region")
 @click.option("--aws-cluster", "-a", default="production", help="aws k8s cluster name")
 @click.option("--deploy-type", "-d", default="gcp", help="type of deploy. Can be gcp, aws or multi")
-def deploy_helm(chart_name, gcp_project, gcp_credentials, tag, deploy_env, gcp_cluster, aws_credentials_csv_file, aws_region, aws_cluster, deploy_type):
+@click.option("--gcp-zone", "-r", default="us-central1-a", help="the gcp zone")
+def deploy_helm(chart_name, gcp_project, gcp_credentials, tag, deploy_env, gcp_cluster, aws_credentials, aws_region, aws_cluster, deploy_type, gcp_zone):
     """Tool to deploy k8s by helm."""
     try:
         click.echo(f"Trying deploy {chart_name}, gcp project: {gcp_project}, gcp-cred: {gcp_credentials}, tag: {tag}, deploy env: {deploy_env}")
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_credentials
         setup_gcp_account(gcp_project, gcp_credentials)
         chart_file = helm_package_and_push(chart_name, tag, deploy_env)
-        if deploy_type in ["GCP", "MULTI"]:
-            install_helm_on_gcp(gcp_project, gcp_cluster, chart_name, chart_file)
-        if deploy_type in ["AWS", "MULTI"]:
-           install_helm_on_aws(aws_credentials_csv_file, aws_region, aws_cluster, chart_name, chart_file)
+        if deploy_type in ["gcp", "multi"]:
+            install_helm_on_gcp(gcp_project, gcp_cluster, gcp_zone, chart_name, chart_file)
+        if deploy_type in ["aws", "multi"]:
+           install_helm_on_aws(aws_credentials, aws_region, aws_cluster, chart_name, chart_file)
         click.echo("Image pushed successfully")
     except OSError as e:
         print("Execution failed:", e, file=sys.stderr)
         exit(1)
 
-def install_helm_on_gcp(gcp_project, gcp_cluster, chart_name, chart_file):
-    sh(f"gcloud container clusters get-credentials {gcp_cluster}-cluster --zone us-central1-a --project ${gcp_project}")
+def install_helm_on_gcp(gcp_project, gcp_cluster, gcp_zone, chart_name, chart_file):
+    sh(f"gcloud container clusters get-credentials {gcp_cluster} --zone {gcp_zone} --project {gcp_project}")
     install_helm(chart_name, chart_file)
 
 def install_helm_on_aws(aws_credentials, aws_region, aws_cluster, chart_name, chart_file):
@@ -44,12 +45,13 @@ def install_helm_on_aws(aws_credentials, aws_region, aws_cluster, chart_name, ch
     install_helm(chart_name, chart_file)
 
 def install_helm(chart_name, chart_file):
-    click.echo("Deploy on GCP")
+    click.echo(f"Helm deploy {chart_name} {chart_file}")
     sh(f"helm upgrade {chart_name} {chart_file} --install")
 
 def helm_package_and_push(chart_name, app_version, deploy_env):
     gcs_bucket_name = f"gs://volanty-charts-{deploy_env}/{chart_name}"
     base_chart = f"./helm/{chart_name}"
+    os.rename(f"{base_chart}/values-{deploy_env}.yaml", f"{base_chart}/values.yaml")
     click.echo("Initializing helm repo")
     sh(f"helm gcs init {gcs_bucket_name}")
     click.echo(f"Adding gcs bucket repo {gcs_bucket_name}")
--- scripts/deployer/utils.py
+++ scripts/deployer/utils.py
@@ -6,12 +6,11 @@ def sh(cmd):
 def setup_gcp_account(gcp_project, gcp_credentials_file):
     sh(f"gcloud auth activate-service-account --key-file {gcp_credentials_file} --quiet")
     sh(f"gcloud config set project {gcp_project} --quiet")
-    sh(f"gcloud container clusters get-credentials elasticsearch-cluster --zone us-central1-a --project {gcp_project}")
 
 def push_docker_image(base, name, tag, args):
     sh("gcloud auth configure-docker --quiet")
-    sh(f"docker build -t ${base}/{name}:{tag} {args} .")
-    sh(f"docker push ${base}/{name}:{tag}")
+    sh(f"docker build -t {base}/{name}:{tag} {args} .")
+    sh(f"docker push {base}/{name}:{tag}")
 
 def setup_aws_account(aws_credentials_csv_file, region):
     sh(f"aws configure import --csv file://{aws_credentials_csv_file}")
