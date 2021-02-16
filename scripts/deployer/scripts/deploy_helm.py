012e27a7025f1e26c4977b324b13543a5e39cb98
Inaiat H. Moraes <inaiat@gmail.com>
2021-02-16 11:23:56 -0300

    refactoring

--- scripts/deployer/scripts/deploy_helm.py
+++ scripts/deployer/scripts/deploy_helm.py
@@ -7,30 +7,28 @@ import os
 
 from ..utils import *
 
-GOOGLE_APPLICATION_CREDENTIALS="/tmp/key-file.json"
-
 @click.command(context_settings={'show_default': True})
 @click.argument("chart-name")
 @click.argument("gcp-project")
-@click.argument("gcp-key")
+@click.argument("gcp-credentials", default=os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+@click.argument("tag", default=os.getenv("BITBUCKET_COMMIT"))
+@click.argument("deploy-env", default=os.getenv("BITBUCKET_DEPLOYMENT_ENVIRONMENT"))
 @click.option("--gcp-cluster", "-g", default="elasticsearch-cluster", help="gcp k8s cluster name")
-@click.option("--aws-key", "-k", default="K8S_PRD_AWS", help="the key that contain aws secrets")
-@click.option("--aws-cluster", "-c", default="production", help="aws k8s cluster name")
-@click.option("--tag", "-t", default="BITBUCKET_COMMIT", help="tag to be applied to this deploy")
-@click.option("--deploy_env", "-e", default="BITBUCKET_DEPLOYMENT_ENVIRONMENT", help="environment to be applied to this deploy")
+@click.option("--aws-credentials-csv-file", "-c", default=os.getenv("AWS_K8S_CREDENTIALS"), help="the aws credentials csv file")
+@click.option("--aws-region", "-r", default="us-west-2", help="the aws region")
+@click.option("--aws-cluster", "-a", default="production", help="aws k8s cluster name")
 @click.option("--deploy-type", "-d", default="gcp", help="type of deploy. Can be gcp, aws or multi")
-def deploy_helm(chart_name, gcp_project, gcp_key, aws_key, gcp_cluster, aws_cluster, tag, deploy_env, deploy_type):
+def deploy_helm(chart_name, gcp_project, gcp_credentials, tag, deploy_env, gcp_cluster, aws_credentials_csv_file, aws_region, aws_cluster, deploy_type):
     """Tool to deploy k8s by helm."""
     try:
-        gcp_key_base64 = os.getenv(gcp_key, "")
-        if len(gcp_key_base64) == 0: raise ValueError("The value of gcp-key is empty.")
-        sh("echo \"{gcp_key}\" | base64 -d >> {key_file}".format(gcp_key=gcp_key_base64, key_file=GOOGLE_APPLICATION_CREDENTIALS))
-        setup_gcp_account(gcp_project, GOOGLE_APPLICATION_CREDENTIALS)
+        click.echo(f"Trying deploy {chart_name}, gcp project: {gcp_project}, gcp-cred: {gcp_credentials}, tag: {tag}, deploy env: {deploy_env}")
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_credentials
+        setup_gcp_account(gcp_project, gcp_credentials)
         chart_file = helm_package_and_push(chart_name, tag, deploy_env)
         if deploy_type in ["GCP", "MULTI"]:
             install_helm_on_gcp(gcp_project, gcp_cluster, chart_name, chart_file)
         if deploy_type in ["AWS", "MULTI"]:
-           install_helm_on_aws(aws_key, aws_cluster, chart_name, chart_file)
+           install_helm_on_aws(aws_credentials_csv_file, aws_region, aws_cluster, chart_name, chart_file)
         click.echo("Image pushed successfully")
     except OSError as e:
         print("Execution failed:", e, file=sys.stderr)
@@ -40,11 +38,8 @@ def install_helm_on_gcp(gcp_project, gcp_cluster, chart_name, chart_file):
     sh(f"gcloud container clusters get-credentials {gcp_cluster}-cluster --zone us-central1-a --project ${gcp_project}")
     install_helm(chart_name, chart_file)
 
-
-def install_helm_on_aws(aws_key, aws_cluster, chart_name, chart_file):
-    aws_key_env = os.getenv(aws_key, "")
-    if len(aws_key_env) == 0: raise ValueError("The value of aws key is empty.")
-    setup_aws_account(aws_key_env)
+def install_helm_on_aws(aws_credentials, aws_region, aws_cluster, chart_name, chart_file):
+    setup_aws_account(aws_credentials, aws_region)
     sh(f"aws eks update-kubeconfig --name {aws_cluster}")
     install_helm(chart_name, chart_file)
 
@@ -54,7 +49,7 @@ def install_helm(chart_name, chart_file):
 
 def helm_package_and_push(chart_name, app_version, deploy_env):
     gcs_bucket_name = f"gs://volanty-charts-{deploy_env}/{chart_name}"
-    base_chart = "./helm/${chart_name}"
+    base_chart = f"./helm/{chart_name}"
     click.echo("Initializing helm repo")
     sh(f"helm gcs init {gcs_bucket_name}")
     click.echo(f"Adding gcs bucket repo {gcs_bucket_name}")
@@ -62,8 +57,8 @@ def helm_package_and_push(chart_name, app_version, deploy_env):
     sh(f"helm repo index . --url {gcs_bucket_name}")
     sh(f"helm package --app-version {app_version} {base_chart}")
     result = subprocess.run(f"helm package --app-version {app_version} {base_chart}", shell=True, check=True, capture_output=True)
-    chart_file = result.stdout.decote().split(" ")[-1]
-    click.echo("Pushing $chart_file...")
+    chart_file = result.stdout.decode().split(" ")[-1].strip()
+    click.echo(f"Pushing {chart_file}...")
     sh(f"helm gcs push {chart_file} private --force")
     return chart_file
 
--- scripts/deployer/utils.py
+++ scripts/deployer/utils.py
@@ -13,12 +13,6 @@ def push_docker_image(base, name, tag, args):
     sh(f"docker build -t ${base}/{name}:{tag} {args} .")
     sh(f"docker push ${base}/{name}:{tag}")
 
-def setup_aws_account(aws_credential_string):
-    aws_access_key_id, aws_secret_access_key, region = aws_credentials_to_dict(aws_credential_string)
-    sh(f"aws configure set aws_access_key_id {aws_access_key_id}")
-    sh(f"aws configure set aws_secret_access_key {aws_secret_access_key}")
+def setup_aws_account(aws_credentials_csv_file, region):
+    sh(f"aws configure import --csv file://{aws_credentials_csv_file}")
     sh(f"aws configure set default.region {region}")    
-
-def aws_credentials_to_dict(aws_credential_string):
-    list_credentials = list(map(lambda x: tuple(x.split("=")), aws_credential_string.split(";")))
-    return dict(list_credentials)
